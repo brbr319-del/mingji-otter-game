5d41ed2df6899c8e10c7d26cd259f3782f43df36
brbr319-del <brbr319@gmail.com>
2026-08-24 13:20:07 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE='mingji-otter-v20-1-3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin)return;
+  if(url.pathname.endsWith('/admin')||url.pathname.endsWith('/admin.html')){
+    event.respondWith(fetch(event.request,{cache:'no-store'}));return;
+  }
+  event.respondWith(
+    fetch(event.request).then(res=>{
+      const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return res;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+  );
+});
